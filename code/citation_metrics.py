c917a52531f9f87247a9cc611d0a8fde10772070
Martin Schlecker <martin.schlecker@mailbox.org>
2024-02-27 19:07:15 -0700

    plot papers

--- code/citation_metrics.py
+++ code/citation_metrics.py
@@ -16,17 +16,14 @@ max_year = 2023
 
 fig, ax = plt.subplots(figsize=(6, 4))
 
+def plot_papers(ax):
+    """Plot publications over time."""
+    p = pd.read_csv('metrics-papers.csv', delimiter=', ', engine='python')
+    p = p[p['Year'].between(min_year, max_year)]
 
-def plot_cites(ax):
-    """Plot citations over time."""
-
-    c = pd.read_csv('metrics-citations.csv', delimiter=', ', engine='python')
-    c = c[c['Year'].between(min_year, max_year)]
-
-    cdf = np.cumsum(c.Total)
-    # bins = 0.5 * (bin_edges[1:] + bin_edges[:-1])
-    ax.plot(c.Year, cdf, ".", color="C2", ms=3)
-    ax.plot(c.Year, cdf, "-", color="C4", lw=3, alpha=0.5)
+    cdf = np.cumsum(p.Refereed)
+    ax.plot(p.Year, cdf, ".", color="C7", ms=3)
+    ax.plot(p.Year, cdf, "-", color="C7", lw=3, alpha=0.7)
     plt.setp(
         ax.get_xticklabels(), rotation=30, fontsize=10, fontproperties=lato, alpha=0.75
     )
@@ -36,8 +33,32 @@ def plot_cites(ax):
     ax.xaxis.set_major_locator(MaxNLocator(integer=True, nbins=4))
     for tick in ax.get_xticklabels() + ax.get_yticklabels():
         tick.set_fontsize(10)
-    ax.set_ylabel("citations", fontsize=16)
-    ax.set_xlabel("year", fontsize=16)
+    ax.set_ylabel("refereed\npublications", fontsize=15)
+    ax.set_xlabel("year", fontsize=15)
+    # ax.set_xlim(min_year, datetime.now().year + datetime.now().month / 12)
+    ax.set_xlim(min_year, max_year + 0.5)
+
+
+def plot_cites(ax):
+    """Plot citations over time."""
+
+    c = pd.read_csv('metrics-citations.csv', delimiter=', ', engine='python')
+    c = c[c['Year'].between(min_year, max_year)]
+
+    cdf = np.cumsum(c.Total)
+    ax.plot(c.Year, cdf, ".", color="C4", ms=3)
+    ax.plot(c.Year, cdf, "-", color="C4", lw=3, alpha=0.7)
+    plt.setp(
+        ax.get_xticklabels(), rotation=30, fontsize=10, fontproperties=lato, alpha=0.75
+    )
+    plt.setp(
+        ax.get_yticklabels(), rotation=30, fontsize=10, fontproperties=lato, alpha=0.75
+    )
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True, nbins=4))
+    for tick in ax.get_xticklabels() + ax.get_yticklabels():
+        tick.set_fontsize(10)
+    ax.set_ylabel("citations", fontsize=15)
+    ax.set_xlabel("year", fontsize=15)
     # ax.set_xlim(min_year, datetime.now().year + datetime.now().month / 12)
     ax.set_xlim(min_year, max_year + 0.5)
 
@@ -58,17 +79,17 @@ def plot_metrics(ax):
         ax.plot(xi, yi, "-", color="C%d" % i, lw=3, alpha=0.75, label=metric)
 
     plt.setp(
-        ax.get_xticklabels(), rotation=30, fontsize=10, fontproperties=lato, alpha=0.75
+        ax.get_xticklabels(), rotation=30, fontsize=10, fontproperties=lato, alpha=0.7
     )
     plt.setp(
-        ax.get_yticklabels(), rotation=30, fontsize=10, fontproperties=lato, alpha=0.75
+        ax.get_yticklabels(), rotation=30, fontsize=10, fontproperties=lato, alpha=0.7
     )
     ax.xaxis.set_major_locator(MaxNLocator(integer=True, nbins=4))
     for tick in ax.get_xticklabels() + ax.get_yticklabels():
         tick.set_fontsize(10)
     ax.legend(loc="upper left", fontsize=8)
-    ax.set_ylabel("index", fontsize=16)
-    ax.set_xlabel("year", fontsize=16)
+    ax.set_ylabel("index", fontsize=15)
+    ax.set_xlabel("year", fontsize=15)
     # ax.set_xlim(min_year, datetime.now().year + datetime.now().month / 12)
     ax.set_xlim(min_year, max_year + 0.5)
     ax.legend()
@@ -77,13 +98,14 @@ def plot_metrics(ax):
 
 def make_plots():
     fig, axs = plt.subplots(1, 3, figsize=(11, 2))
-    fig.subplots_adjust(wspace=0.6)
-    # plot_papers(axs[0])
+    fig.subplots_adjust(wspace=0.4)
+    plot_papers(axs[0])
     plot_cites(axs[1])
     plot_metrics(axs[2])
-    for axis in axs[4:]:
-        axis.axis("off")
+    for axis in axs:
+        axis.spines[['right', 'top']].set_visible(False)
 
+    fig.suptitle("Publication metrics", fontsize=16, y=1.1)
     plt.show()
     fig.savefig("metrics.pdf", bbox_inches="tight")
 
